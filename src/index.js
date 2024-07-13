4560fa9dffad1c6b615fd2401c8505a49756ab90
gkambic <gkambic@gmail.com>
2024-07-13 13:13:34 -0300

    Se agrega constante con valor del puerto

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 const path = require('path');
-
+const PORT = 8080;
 //initializations
 const app = express();
 
